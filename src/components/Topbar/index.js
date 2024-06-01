cd81b9b45db6553c2ea8c5c45262d1c162304263
Durgesh2601 <dk829445@gmail.com>
2024-06-01 19:06:28 +0530

    feat: right search bar

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -1,10 +1,26 @@
 import Tabs from "../Tabs";
+import search from "../../assets/search.svg";
 import "./index.css";
 
 const Topbar = () => {
   return (
     <div className="topbar">
-      <Tabs />
+      <div className="left">
+        <Tabs />
+      </div>
+      <div className="right">
+        <div className="topbar-right-search">
+          <div className="topbar-input-container">
+            <span className="icon-start">
+              <img src={search} alt="search" />
+            </span>
+            <input
+              type="text"
+              placeholder="Search Bounties, Profiles, and more..."
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
